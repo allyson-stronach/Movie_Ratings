e34c9bb7417fec5658e7bacf2d54e3487939a13c
Hackbright Student <info@hackbrightacademy.com>
2014-10-24 17:36:03 -0700

    working on timestamp from datetime

--- model.py
+++ model.py
@@ -1,13 +1,53 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker
+
+ENGINE = None
+Session = None
 
 Base = declarative_base()
+#each class below (User, Movie, Rating) is basically an instance of Base (a super class). The instructions said this was sort of "magical"
 
 ### Class declarations go here
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key = True)
+    age = Column(Integer, nullable=True)
+    gender = Column(String, nullable=True)
+    zip_code = Column(String(15), nullable=True)
+    email = Column(String (64), nullable=True)
+    password = Column(String(64), nullable=True)
+
+class Movie(Base):
+    __tablename__ = "movies"
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String (64), nullable=True)
+    released_at = Column(DateTime, nullable=True)
+    imdb_url = Column(String, nullable=True)
+
+class Rating(Base):
+    __tablename__ = "ratings"
+
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, nullable = True)
+    movie_id = Column(Integer, nullable=True)
+    rating = Column(Integer, nullable=True)
+    timestamp = Column(DateTime, nullable=True)
 
 
 ### End class declarations
+
+
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE =create_engine("sqlite:///ratings.db", echo=True)
+    Session = sessionmaker(bind=ENGINE)
+    return Session()
 
 def main():
     """In case we need this for something"""
--- seed.py
+++ seed.py
@@ -1,5 +1,13 @@
 import model
 import csv
+u_data = open('/home/user/src/ratings/seed_data/u.data', 'r')
+u_item = open('/home/user/src/ratings/seed_data/u.item', 'r')
+u_user = open('/home/user/src/ratings/seed_data/u.user', 'r')
+
+
+
+
+# new_user = User(age='%d', gender='%s', zip_code='%s', email='%s', password='%s') % (x, g, z, a, b) 
 
 def load_users(session):
     # use u.user
@@ -10,12 +18,27 @@ def load_movies(session):
     pass
 
 def load_ratings(session):
-    # use u.data
-    pass
+    u_data_reader = csv.reader(u_data, delimiter = '\t')
+    for line in u_data_reader:
+        # without the delimiter specified above, this would also work to split the line:
+            #sl = line[0].split() 
+
+        #new_rating = model.Rating(id, movie_id, user_id, rating)
+        dt = int(line[3])
+        '881250949'
+
+        new_rating = model.Rating()
+        new_rating.user_id = int(line[0])
+        new_rating.movie_id = int(line[1])
+        new_rating.rating = int(line[2])
+        new_rating.timestamp = int(line[3])
+        session.add(new_rating)
+        session.commit()
+        
 
 def main(session):
-    # You'll call each of the load_* functions with the session as an argument
-    pass
+    load_ratings(session)
+
 
 if __name__ == "__main__":
     s= model.connect()
