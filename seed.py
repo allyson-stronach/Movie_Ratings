78ae4b45779582b3ae237ac72f74b699269f5912
Hackbright Student <info@hackbrightacademy.com>
2014-10-24 18:15:46 -0700

    Initial commit - working through datetime conversion Part 1

--- seed.py
+++ seed.py
@@ -1,13 +1,10 @@
 import model
 import csv
+import datetime
 u_data = open('/home/user/src/ratings/seed_data/u.data', 'r')
 u_item = open('/home/user/src/ratings/seed_data/u.item', 'r')
 u_user = open('/home/user/src/ratings/seed_data/u.user', 'r')
 
-
-
-
-# new_user = User(age='%d', gender='%s', zip_code='%s', email='%s', password='%s') % (x, g, z, a, b) 
 
 def load_users(session):
     # use u.user
@@ -17,24 +14,28 @@ def load_movies(session):
     # use u.item
     pass
 
+def to_datetime_from_timestamp(timestamp):
+    return (datetime.datetime.fromtimestamp(int(timestamp))).strftime('%Y-%m-%d %H:%M:%S')
+
 def load_ratings(session):
     u_data_reader = csv.reader(u_data, delimiter = '\t')
     for line in u_data_reader:
         # without the delimiter specified above, this would also work to split the line:
             #sl = line[0].split() 
-
-        #new_rating = model.Rating(id, movie_id, user_id, rating)
-        dt = int(line[3])
-        '881250949'
+        timestamp = int(line[3])
+        datetime = to_datetime_from_timestamp(timestamp)
 
         new_rating = model.Rating()
         new_rating.user_id = int(line[0])
         new_rating.movie_id = int(line[1])
         new_rating.rating = int(line[2])
-        new_rating.timestamp = int(line[3])
+        new_rating.timestamp = datetime
         session.add(new_rating)
         session.commit()
-        
+
+        #CURRENT ERROR
+        #(original cause: TypeError: SQLite DateTime type only accepts Python datetime and date objects as input.) u'INSERT INTO ratings (user_id, movie_id, rating, timestamp) VALUES (?, ?, ?, ?)' [{'movie_id': 242, 'user_id': 196, 'timestamp': '1997-12-04 07:55:49', 'rating': 3}]
+
 
 def main(session):
     load_ratings(session)
