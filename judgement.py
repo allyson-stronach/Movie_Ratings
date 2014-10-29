aa380d73eefa24c1f42dcc86dc4ac4904eb8bb2e
Hackbright Student <info@hackbrightacademy.com>
2014-10-29 13:01:47 -0700

    Completed links to list users ratings and movies

--- judgement.py
+++ judgement.py
@@ -5,24 +5,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    user_list = model.session.query(model.User).limit(5).all()
+    user_list = model.session.query(model.User).limit(10).all()
     #in the following line: the orange user_list is the one that the html can read, and the white user_list is the python variable that we're manipulating in our route.
     return render_template("user_list.html", user_list=user_list)
 
-@app.route("/user/<int:id>")
-def movie_ratings_by_user(id):
-
-    #write a line that gets all movie ratings rated by single user
-    #model like line below:
-
-    ratings_list = model.session.query(model.Rating).WTF_DO_I_PUT_HERE
-
-    # it makes sense to put something like SELECT * FROM Ratings WHERE id = id 
-    # maybe it is filter_by()  <-search this in hackbright exercise page AND/OR SQL Alchemy documentation
-
-    return render_template("movie_rating_list.html", id=id, ratings_list=ratings_list)
-
-    pass 
+@app.route("/user/<int:user_id>")
+def get_ratings_list(user_id):
+    ratings_list = model.session.query(model.Rating).filter_by(user_id = user_id).all()
+    #^this returns a list of rating objects, each of which has the associated columns
+    return render_template("movie_rating_list.html", user_id=user_id, ratings_list=ratings_list)
 
 if __name__== "__main__":
     app.run(debug = True)
--- model.py
+++ model.py
@@ -35,10 +35,12 @@ class Movie(Base):
 
     id = Column(Integer, primary_key = True)
     # we don't need the line below! in this table, there will only be one movie--remake db???
-    #movie_id = Column(Integer, nullable=True)
+    movie_id = Column(Integer, nullable=True)
     name = Column(String (64), nullable=True)
     released_at = Column(DateTime, nullable=True)
     imdb_url = Column(String (200), nullable=True)
+
+    #Movie table is backreferenced from Rating table
 
 class Rating(Base):
     __tablename__ = "ratings"
@@ -50,7 +52,6 @@ class Rating(Base):
     timestamp = Column(DateTime, nullable=True)
 
     user = relationship("User", backref=backref("ratings", order_by=id))
-    #added the line below to access movie name from ratings table; not sure about the order_by=id part...see above for id issue
     movie = relationship("Movie", backref=backref("ratings", order_by=id))
 
 
